ff2df26c5beb84cae5346451c8d219d8292a5875
SamJamCul <samuel.culley@digital.education.gov.uk>
2020-08-21 12:01:41 +0100

    initial commit

---
+++ puppeteer/headlines.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer')
+
+const main = async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(`https://www.express.co.uk/${process.argv[2]}`)
+  let headlines = await page.$$eval("h4", elements=> elements.map(item=>item.textContent.trim()))
+  headlines = [...new Set(headlines)]
+  const fs = require('fs');
+  let file = fs.createWriteStream('headlines.txt');
+  headlines.forEach(value => file.write(`${value}\n`));
+  file.end();
+  await browser.close()
+}
+
+main()
